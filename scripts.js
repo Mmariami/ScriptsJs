1f70e362c33cbd8755c4577d9373757903172462
Mmariami <mxarshiladze29@gmail.com>
2021-01-14 01:13:10 +0400

    task 6 complete

--- scripts.js
+++ scripts.js
@@ -1,4 +1,3 @@
-    // 5 Task //
 
     var person = {
         name : "john" ,
@@ -10,3 +9,17 @@
 // 4 task //
  let names = ['name', 25, 50, 'age', 'surname' , [50,500] ];
  console.log(names[5][1]);
+
+// 3 task //
+
+var massive3 = [1,45,55,109,908,704, 88, 703];
+// task 8 //
+
+var fruits = ['orange', 'banana' , 'pear'];
+console.log(fruits.length)
+
+// task 6//
+var dates = [11,27,8,31,9,24,30];
+for (let i = 0; i<7; i++) {
+    console.log(dates[i]);
+}
