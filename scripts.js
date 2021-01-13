87e9c5d1ad093c7b8ee854c077f2325bb370334b
Mmariami <mxarshiladze29@gmail.com>
2021-01-13 14:44:16 +0400

    task 5 complete

---
+++ scripts.js
@@ -0,0 +1,11 @@
+
+    // 5 Task //
+
+    var person = {
+        name : "john" ,
+        lastname : "doe",
+        age : 50,
+        eyecolour : "blue"
+    };
+    console.log(person.eyecolour)
+
